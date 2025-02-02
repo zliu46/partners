d31df71762cb6e92065c729f46fbd3ccc29a05e8
zhouliu <zliu2@seattleu.edu>
2025-02-02 01:41:37 -0800

    Set up Flutter App for Firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:partners/baby_task_page.dart';
 import 'package:partners/calendar_page.dart';
 import 'package:partners/create_task_page.dart';
+import 'package:partners/firebase_options.dart';
 import 'package:partners/profile_page.dart';
 import 'package:partners/sign_up_page.dart';
 import 'package:partners/task_categories_page.dart';
@@ -12,7 +14,11 @@ import 'home_page.dart';
 import 'login_page.dart';
 
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
