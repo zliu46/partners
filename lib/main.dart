329d31a8401434d607c365c50d7a19f222d4fdb9
zhouliu <zliu2@seattleu.edu>
2025-02-24 07:36:48 -0800

    Delete partnerships_service.dart and people_page.dart. Update imports in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:partners/provider/auth_service.dart';
 import 'package:partners/provider/database_service.dart';
-import 'package:partners/provider/partnerships_service.dart';
 import 'package:partners/provider/task_provider.dart';
 import 'package:partners/pages/calendar_page.dart';
 import 'package:partners/pages/create_task_page.dart';
