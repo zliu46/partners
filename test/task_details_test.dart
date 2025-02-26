ee27cd13c0891fd7a7d2fa63212ddd102aed978f
josephxu1 <josephxu35@gmail.com>
2025-02-25 23:02:04 -0800

    fix task_details test

--- test/task_details_test.dart
+++ test/task_details_test.dart
@@ -28,20 +28,50 @@ void main() {
     }
     );
 
-    test('Should handle task with null endTime', () {
+    test('Create a TaskDetails instance using fromMap factory', ()
+    {
+      final task = TaskDetails.fromMap({
+        'id': '1',
+        'title': 'Wash baby bottles',
+        'category': 'Baby',
+        'description': 'Clean the baby bottles and put them in the serializer',
+        'createdBy': 'Zhou',
+        'startTime': DateTime(2025, 2, 20, 10, 0),
+        'endTime': DateTime(2025, 2, 20, 10, 20),
+        'assignedTo': '',
+        'isCompleted': false
+      });
+      expect(task.id, '1');
+      expect(task.title, 'Wash baby bottles');
+      expect(task.category, 'Baby');
+      expect(task.description, 'Clean the baby bottles and put them in the serializer');
+      expect(task.createdBy, 'Zhou');
+      expect(task.startTime, DateTime(2025, 2, 20, 10, 0));
+      expect(task.endTime, DateTime(2025, 2, 20, 10, 20));
+      expect(task.isCompleted, false);
+    }
+    );
+
+    test("Ensure Taskdetail's .changeCompletion() function works", ()
+    {
       final task = TaskDetails(
-        id: '2',
-        title: 'Takeout trash',
-        category: 'Chores',
-        description: 'Takeout the trash by the door when you leave the house',
-        createdBy: 'Joseph',
-        startTime: DateTime(2025, 2, 20, 10, 0),
-        endTime: DateTime.now(),
-        assignedTo: '',
-        isCompleted: false
+          id: '1',
+          title: 'Wash baby bottles',
+          category: 'Baby',
+          description: 'Clean the baby bottles and put them in the serializer',
+          createdBy: 'Zhou',
+          startTime: DateTime(2025, 2, 20, 10, 0),
+          endTime: DateTime(2025, 2, 20, 10, 20),
+          assignedTo: '',
+          isCompleted: false
       );
 
-      expect(task.endTime, null);
-    });
+      expect(task.isCompleted, false);
+
+      task.changeCompletion();
+      expect(task.isCompleted, true);
+
+    }
+    );
   });
 }
