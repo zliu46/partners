beeba65d8dc893b14a09e5e8a42e27a824e09084
zhouliu <zliu2@seattleu.edu>
2025-03-10 11:27:17 -0700

    Fixed withValue

--- lib/widgets/task_item_card.dart
+++ lib/widgets/task_item_card.dart
@@ -55,7 +55,7 @@ class _TaskItemCardState extends State<TaskItemCard> {
             borderRadius: BorderRadius.circular(16.0),
             boxShadow: [
               BoxShadow(
-                color: Colors.black.withOpacity(0.08),
+                color: Colors.black.withValues(alpha: 0.08),
                 blurRadius: 6,
                 offset: const Offset(2, 4),
               ),
