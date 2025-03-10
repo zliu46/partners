401d35d774240610bf24b5d62c22f3c5f9ac7e59
zhouliu <zliu2@seattleu.edu>
2025-03-10 08:05:20 -0700

    Created splash screen to display animation when the user starts the app.

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/welcome');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Lottie.asset(
+          'lib/assets/Animation.json',
+          width: 250,
+          height: 250,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
